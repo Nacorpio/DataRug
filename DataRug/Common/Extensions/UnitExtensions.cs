de0cde53c5d47a58730a2d4080caeeff95268a7b
Nac <teampavilion@gmail.com>
2020-09-19 13:37:41 +0200

    [^] Simplified and refactored a lot of the methods in the UnitExtensions class [^] Documented the UnitExtensions class

--- DataRug/Common/Extensions/UnitExtensions.cs
+++ DataRug/Common/Extensions/UnitExtensions.cs
@@ -8,26 +8,85 @@ namespace DataRug.Common.Extensions
 
     public static class UnitExtensions
     {
-        public delegate UnitValue<TValue, TUnit> BinaryDelegate<TValue, TUnit>
-            (UnitValue<TValue, TUnit> lhs, UnitValue<TValue, TUnit> rhs)
-            where TUnit : struct
-            where TValue : struct, IEquatable<TValue>;
+        /// <summary>
+        /// Represents a binary operation delegate that takes a left- and right-hand operator.
+        /// </summary>
+        /// <typeparam name="TValue">The value type.</typeparam>
+        /// <typeparam name="TUnit">The unit type.</typeparam>
+        /// <param name="lhs">The left-hand operator.</param>
+        /// <param name="rhs">The right-hand operator.</param>
+        /// <returns>The resulting value of the binary operation, if successful; otherwise, <c>default</c>.</returns>
+        public delegate UnitValue <TValue, TUnit> BinaryDelegate <TValue, TUnit>
+        (
+            UnitValue <TValue, TUnit> lhs,
+            UnitValue <TValue, TUnit> rhs)
 
-        public delegate UnitValue<TValue, TUnit> UnaryDelegate<TValue, TUnit>(UnitValue<TValue, TUnit> lhs)
             where TUnit : struct
-            where TValue : struct, IEquatable<TValue>;
+            where TValue : struct, IEquatable <TValue>;
 
+        /// <summary>
+        /// Represents a unary operation delegate that takes a left-hand operator.
+        /// </summary>
+        /// <typeparam name="TValue">The value type.</typeparam>
+        /// <typeparam name="TUnit">The unit type.</typeparam>
+        /// <param name="lhs">The left-hand operator.</param>
+        /// <returns>The resulting value of the unary operation, if successful; otherwise, <c>default</c>.</returns>
+        public delegate UnitValue <TValue, TUnit> UnaryDelegate <TValue, TUnit>
+        (
+            UnitValue <TValue, TUnit> lhs)
+
+            where TUnit : struct
+            where TValue : struct, IEquatable <TValue>;
+
+        /// <summary>
+        /// Returns a <see cref="IUnitConverter{TValue,TUnit}"/> matching the specified <see cref="TUnit"/>.
+        /// </summary>
+        /// <typeparam name="TValue">The value type.</typeparam>
+        /// <typeparam name="TUnit">The unit type.</typeparam>
+        /// <returns>The corresponding unit converter, if found; otherwise, <c>default</c>.</returns>
+        internal static IUnitConverter <TValue, TUnit> GetConverter <TValue, TUnit>()
+            where TUnit : struct
+            where TValue : struct, IEquatable <TValue> 
+        =>
+            typeof (TUnit) == typeof (MassUnit)
+                ? (IUnitConverter <TValue, TUnit>) Mass.Instance
+            : typeof (TUnit) == typeof (TimeUnit)
+                ? (IUnitConverter <TValue, TUnit>) Time.Instance
+                : default;
+
+        /// <summary>
+        /// Performs a unary operator on the specified value and returns the result.
+        /// </summary>
+        /// <typeparam name="TValue">The value type.</typeparam>
+        /// <typeparam name="TUnit">The unit type.</typeparam>
+        /// <param name="lhs">The left-hand operator.</param>
+        /// <param name="func">The right-hand operator.</param>
+        /// <returns>The resulting value of the unary operation, if successful; otherwise, <c>default</c>.</returns>
         public static UnitValue<TValue, TUnit> Unary<TValue, TUnit>
         (
             this UnitValue<TValue, TUnit> lhs,
             UnaryDelegate<TValue, TUnit> func
         )
             where TUnit : struct
-            where TValue : struct, IEquatable<TValue>
-        {
-            return Unit.New(func(lhs).Value, lhs.Unit) ?? default;
-        }
+            where TValue : struct, IEquatable<TValue> =>
+            Unit.New(
+                func(
+                        lhs
+                    )
+                   .Value,
+                lhs.Unit
+            )
+            ?? default;
 
+        /// <summary>
+        /// Performs a binary operation on the specified values and returns the result.
+        /// </summary>
+        /// <typeparam name="TValue">The value type.</typeparam>
+        /// <typeparam name="TUnit">The unit type.</typeparam>
+        /// <param name="lhs">The left-hand operator.</param>
+        /// <param name="rhs">The right-hand operator.</param>
+        /// <param name="func">The binary function.</param>
+        /// <returns>The resulting value of the binary operation, if successful; otherwise, <c>default</c>.</returns>
         public static UnitValue<TValue, TUnit> Binary<TValue, TUnit>
         (
             this UnitValue<TValue, TUnit> lhs,
@@ -35,211 +94,144 @@ namespace DataRug.Common.Extensions
             BinaryDelegate<TValue, TUnit> func
         )
             where TValue : struct, IEquatable<TValue>
-            where TUnit : struct
-        {
-            return Unit.New(func(lhs, rhs).Value, lhs.Unit) ?? default;
-        }
+            where TUnit : struct =>
+            Unit.New(
+                func(
+                        lhs,
+                        rhs
+                    )
+                   .Value,
+                lhs.Unit
+            )
+            ?? default;
 
-        public static IUnitConverter<TValue, TUnit> GetUnitConverter<TValue, TUnit>()
-            where TUnit : struct
-            where TValue : struct, IEquatable<TValue>
-        {
-            if (typeof(TUnit) == typeof(MassUnit))
-            {
-                return (IUnitConverter<TValue, TUnit>)Mass.Instance;
-            }
-
-            if (typeof(TUnit) == typeof(TimeUnit))
-            {
-                return (IUnitConverter<TValue, TUnit>)Time.Instance;
-            }
-
-            return default;
-        }
-
+        /// <summary>
+        /// Converts a <see cref="UnitValue{TValue,TUnit}"/> to the specified unit.
+        /// </summary>
+        /// <typeparam name="TValue">The value type.</typeparam>
+        /// <typeparam name="TUnit">The unit type.</typeparam>
+        /// <param name="unitValue">The unit value to convert.</param>
+        /// <param name="newUnit">The unit to convert the value to.</param>
+        /// <returns>The converted value, if the operation was successful; otherwise, <c>default</c>.</returns>
         public static UnitValue<TValue, TUnit> Convert<TValue, TUnit>
         (
-            this UnitValue<TValue, TUnit> lhs,
+            this UnitValue<TValue, TUnit> unitValue,
             TUnit newUnit
         )
             where TValue : struct, IEquatable<TValue>
             where TUnit : struct =>
-            GetUnitConverter<TValue, TUnit>()
-               .Convert(lhs, newUnit);
+            GetConverter<TValue, TUnit>()
+               .Convert(unitValue, newUnit);
 
+        /// <summary>
+        /// Adds the specified <see cref="UnitValue{TValue,TUnit}"/> objects and returns the result.
+        /// </summary>
+        /// <typeparam name="TValue">The value type.</typeparam>
+        /// <typeparam name="TUnit">The unit type.</typeparam>
+        /// <param name="lhs">The left-hand operator.</param>
+        /// <param name="rhs">The right-hand operator.</param>
+        /// <returns>The resulting value, if the operation was successful; otherwise, <c>default</c>.</returns>
         public static UnitValue<TValue, TUnit> Add<TValue, TUnit>
         (
             this UnitValue<TValue, TUnit> lhs,
             UnitValue<TValue, TUnit> rhs
         )
             where TUnit : struct
-            where TValue : struct, IEquatable<TValue>
-        {
-            return lhs.Binary(
+            where TValue : struct, IEquatable<TValue> =>
+            lhs.Binary(
                 rhs,
-                (left, right) =>
-                {
-                    var rValue = Convert(rhs, lhs.Unit);
-
-                    if (!(left.Value is float leftValue))
-                        return default;
-
-                    if (!(rValue.Value is float rightValue))
-                        return default;
-
-                    return Unit.New(
+                (left, right) => !(left.Value is float leftValue)
+                    ? default
+                    : !(Convert(rhs, lhs.Unit).Value is float rightValue)
+                        ? default
+                        : Unit.New(
                             value: leftValue + rightValue as TValue?,
                             unit: left.Unit
                         )
-                        ?? default;
-                }
+                        ?? default
             );
-        }
 
+        /// <summary>
+        /// Subtracts the specified <see cref="UnitValue{TValue,TUnit}"/> objects and returns the result.
+        /// </summary>
+        /// <typeparam name="TValue">The value type.</typeparam>
+        /// <typeparam name="TUnit">The unit type.</typeparam>
+        /// <param name="lhs">The left-hand operator.</param>
+        /// <param name="rhs">The right-hand operator.</param>
+        /// <returns>The resulting value, if the operation was successful; otherwise, <c>default</c>.</returns>
         public static UnitValue<TValue, TUnit> Subtract<TValue, TUnit>
         (
             this UnitValue<TValue, TUnit> lhs,
             UnitValue<TValue, TUnit> rhs
         )
             where TUnit : struct
-            where TValue : struct, IEquatable<TValue>
-        {
-            return lhs.Binary(
+            where TValue : struct, IEquatable<TValue> =>
+            lhs.Binary(
                 rhs,
-                (left, right) =>
-                {
-                    var rValue = Convert(rhs, lhs.Unit);
-
-                    if (!(left.Value is float leftValue))
-                        return default;
-
-                    if (!(rValue.Value is float rightValue))
-                        return default;
-
-                    return Unit.New(
+                (left, right) => !(left.Value is float leftValue)
+                    ? default
+                    : !(Convert(rhs, lhs.Unit).Value is float rightValue)
+                        ? default
+                        : Unit.New(
                             value: leftValue - rightValue as TValue?,
                             unit: left.Unit
                         )
-                        ?? default;
-                }
+                        ?? default
             );
-        }
 
+        /// <summary>
+        /// Multiplies the specified <see cref="UnitValue{TValue,TUnit}"/> objects and returns the result.
+        /// </summary>
+        /// <typeparam name="TValue">The value type.</typeparam>
+        /// <typeparam name="TUnit">The unit type.</typeparam>
+        /// <param name="lhs">The left-hand operator.</param>
+        /// <param name="rhs">The right-hand operator.</param>
+        /// <returns>The resulting value, if the operation was successful; otherwise, <c>default</c>.</returns>
         public static UnitValue<TValue, TUnit> Multiply<TValue, TUnit>
         (
             this UnitValue<TValue, TUnit> lhs,
             UnitValue<TValue, TUnit> rhs
         )
             where TUnit : struct
-            where TValue : struct, IEquatable<TValue>
-        {
-            return lhs.Binary(
+            where TValue : struct, IEquatable<TValue> => lhs.Binary(
                 rhs,
-                (left, right) =>
-                {
-                    var rValue = Convert(rhs, lhs.Unit);
-
-                    if (!(left.Value is float leftValue))
-                        return default;
-
-                    if (!(rValue.Value is float rightValue))
-                        return default;
-
-                    return Unit.New(
+                (left, right) => !(left.Value is float leftValue)
+                    ? default
+                    : !(Convert(rhs, lhs.Unit).Value is float rightValue)
+                        ? default
+                        : Unit.New(
                             value: leftValue * rightValue as TValue?,
                             unit: left.Unit
                         )
-                        ?? default;
-                }
-            );
-        }
+                        ?? default
+        );
 
+        /// <summary>
+        /// Divides the specified <see cref="UnitValue{TValue,TUnit}"/> objects and returns the result.
+        /// </summary>
+        /// <typeparam name="TValue">The value type.</typeparam>
+        /// <typeparam name="TUnit">The unit type.</typeparam>
+        /// <param name="lhs">The left-hand operator.</param>
+        /// <param name="rhs">The right-hand operator.</param>
+        /// <returns>The resulting value, if the operation was successful; otherwise, <c>default</c>.</returns>
         public static UnitValue<TValue, TUnit> Divide<TValue, TUnit>
         (
             this UnitValue<TValue, TUnit> lhs,
             UnitValue<TValue, TUnit> rhs
         )
             where TUnit : struct
-            where TValue : struct, IEquatable<TValue>
-        {
-            return lhs.Binary(
+            where TValue : struct, IEquatable<TValue> => lhs.Binary(
                 rhs,
-                (left, right) =>
-                {
-                    var rValue = Convert(rhs, lhs.Unit);
-
-                    if (!(left.Value is float leftValue))
-                        return default;
-
-                    if (!(rValue.Value is float rightValue))
-                        return default;
-
-                    return Unit.New(
+                (left, right) => !(left.Value is float leftValue)
+                    ? default
+                    : !(Convert(rhs, lhs.Unit).Value is float rightValue)
+                        ? default
+                        : Unit.New(
                             value: leftValue / rightValue as TValue?,
                             unit: left.Unit
                         )
-                        ?? default;
-                }
+                        ?? default
             );
-        }
-
-        //    public static UnitValue<TValue, TUnit> Add<TValue, TUnit>(
-        //        this UnitValue<TValue, TUnit> lhs,
-        //        UnitValue<TValue, TUnit> rhs)
-
-        //        where TValue : struct, IEquatable<TValue>
-        //        where TUnit : struct
-        //    {
-        //        var rightValue = rhs.Value;
-        //        var rightUnit = rhs.Unit;
-
-        //        switch (lhs)
-        //        {
-        //            case UnitValue<float, MassUnit> massLeft:
-        //                {
-        //                    var leftValue = massLeft.Value ?? 0;
-        //                    var leftUnit = massLeft.Unit;
-
-        //                    if (!(rightUnit is MassUnit rightUnitNew))
-        //                    {
-        //                        return default;
-        //                    }
-
-        //                    if (!(Unit.Mass(rightValue as float?, rightUnitNew) is { } massRight))
-        //                    {
-        //                        return default;
-        //                    }
-
-        //                    var rightValueNew = Mass.Instance.GetUnitConverter(massRight, leftUnit);
-
-        //                    var resultValue = leftValue + rightValueNew.Value as TValue?;
-        //                    var resultUnit = lhs.Unit;
-
-        //                    return Unit.New(resultValue, resultUnit) ?? default;
-        //                }
-
-        //            case UnitValue<float, TimeUnit> timeLeft:
-        //                {
-        //                    var leftValue = timeLeft.Value ?? 0;
-        //                    var leftUnit = timeLeft.Unit;
-
-        //                    if (!(Unit.Time(rightValue as float?, leftUnit) is { } timeRight))
-        //                    {
-        //                        return default;
-        //                    }
-
-        //                    var rightValueNew = Time.Instance.GetUnitConverter(timeRight, leftUnit);
-
-        //                    var resultValue = leftValue + rightValueNew.Value as TValue?;
-        //                    var resultUnit = lhs.Unit;
-
-        //                    return Unit.New(resultValue, resultUnit) ?? default;
-        //                }
-        //        }
-
-        //        return default;
-        //    }
-        //}
 
     }
 
